0b0d7ec0fc61a4f57955c919c7fba38b1bb10177
Alexander <itskilobyte@gmail.com>
2024-09-13 14:36:45 +0500

    Completed 2nd task.

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -6,7 +6,17 @@ public class Task02 {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
-        return "";
+        long parsedInput = Long.parseLong(input);
+
+        if (parsedInput > Integer.MAX_VALUE || parsedInput < Integer.MIN_VALUE) {
+            return "long";
+        } else if (parsedInput > Short.MAX_VALUE || parsedInput < Short.MIN_VALUE) {
+            return "int";
+        } else if (parsedInput > Byte.MAX_VALUE || parsedInput < Byte.MIN_VALUE) {
+            return "short";
+        }
+
+        return "byte";
     }
 
     public static void main(String[] args) {
