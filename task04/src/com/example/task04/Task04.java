8cc29319423c4b1fec0ad8e38cea28db525fd51a
Alexander <itskilobyte@gmail.com>
2024-09-13 14:50:30 +0500

    Completed 4th task.

--- task04/src/com/example/task04/Task04.java
+++ task04/src/com/example/task04/Task04.java
@@ -6,7 +6,19 @@ public class Task04 {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
-        return 0;
+        switch (operation) {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "*":
+                return a * b;
+            case "/":
+                return (float) a / b;
+            default:
+                return 0;
+        }
+
     }
 
     public static void main(String[] args) {
